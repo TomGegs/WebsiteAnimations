a25831e2c7afa385a41b548dc96f021b81a84ec6
TomG <tomgegs@outlook.com>
2024-02-26 10:26:13 +1000

    Delete unused image and SVG files

---
+++ src/assets/hero-images/images.ts
@@ -0,0 +1,48 @@
+import motherDaughterImage1 from './heartwarming_image_of_mother_and_daughter_3.webp';
+import motherDaughterImage2 from './heartwarming_image_of_mother_and_daughter_4.webp';
+import busienssOwner from './image_of_a_local_business_owner_2.webp';
+import busienssOwner2 from './image_of_a_local_business_owner_3.webp';
+import happyGirl from './image_of_a_moment_of_pure_joy_receiving_fresh_water_2.webp';
+import boyWithFavouriteToyBear from './joyful_teenager_holding_favourite_toy.webp';
+import womanReceivingAid from './photo_poignantly_capturing_a_woman_receiving_aid_after_disaster_2.webp';
+import clothingDonation from './warming_photo_captures_boy_receiving_clothing_donation.webp';
+import clothingDonation2 from './warming_photo_captures_boy_receiving_clothing_donation_3.webp';
+
+export const heroImages = [
+    {
+        title: 'Mother & Daughter',
+        thumbnail: motherDaughterImage1,
+    },
+    {
+        title: 'Mother & Daughter',
+        thumbnail: motherDaughterImage2,
+    },
+    {
+        title: 'Local Business Owner',
+        thumbnail: busienssOwner,
+    },
+    {
+        title: 'Local Business Owner',
+        thumbnail: busienssOwner2,
+    },
+    {
+        title: 'Joyful Teenager',
+        thumbnail: happyGirl,
+    },
+    {
+        title: 'Joyful Teenager',
+        thumbnail: boyWithFavouriteToyBear,
+    },
+    {
+        title: 'Receiving Aid',
+        thumbnail: womanReceivingAid,
+    },
+    {
+        title: 'Receiving Clothing Donation',
+        thumbnail: clothingDonation,
+    },
+    {
+        title: 'Receiving Clothing Donation',
+        thumbnail: clothingDonation2,
+    },
+];
--- src/pages/home/HeroSplash.tsx
+++ src/pages/home/HeroSplash.tsx
@@ -1,136 +1,6 @@
 import { HeroParallax } from '../../components/ui/hero-parallax';
-
+import { heroImages } from '../../assets/hero-images/images';
 const HeroSplash = () => {
-    const heroImages = [
-        {
-            title: 'mother and daughter',
-            link: '/',
-            thumbnail:
-                'src/assets/heartwarming_image_of_mother_and_daughter_3.webp',
-        },
-        {
-            title: 'Education',
-            link: '/products/education',
-            thumbnail: 'src/assets/image_of_a_local_business_owner_2.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/heartwarming_image_of_mother_and_daughter_4.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail: 'src/assets/image_of_a_local_business_owner_3.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/image_of_a_moment_of_pure_joy_receiving_fresh_water_2.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail: 'src/assets/joyful_teenager_holding_favourite_toy.webp',
-        },
-
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/photo_poignantly_capturing_a_woman_receiving_aid_after_disaster_2.webp',
-        },
-
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation_3.webp',
-        },
-
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/heartwarming_image_of_mother_and_daughter_4.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail: 'src/assets/image_of_a_local_business_owner_3.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/image_of_a_moment_of_pure_joy_receiving_fresh_water_2.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail: 'src/assets/joyful_teenager_holding_favourite_toy.webp',
-        },
-
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/photo_poignantly_capturing_a_woman_receiving_aid_after_disaster_2.webp',
-        },
-
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation_3.webp',
-        },
-
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation.webp',
-        },
-        {
-            title: 'Health',
-            link: '/products/health',
-            thumbnail:
-                'src/assets/warming_photo_captures_boy_receiving_clothing_donation.webp',
-        },
-    ];
-
     return (
         <section className="h-lvh w-full bg-primary">
             <header>
@@ -139,7 +9,7 @@ const HeroSplash = () => {
             </header>
 
             <HeroParallax products={heroImages} />
-            <div className="bg-black h-20 w-full text-white">Hello</div>
+            <div className="h-20 w-full bg-black text-white">Hello</div>
         </section>
     );
 };
